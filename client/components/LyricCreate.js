df5cddfa6abcd88818d27cc122fdfa4d74428fd1
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-19 19:49:33 -0300

    Lyrical App for GraphQL Course on Udemy

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
+class LyricCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props
+      .mutate({
+        variables: {
+          content: this.state.content,
+          songId: this.props.songId
+        }
+      })
+      .then(() => this.setState({ content: "" }));
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <label>Add a Lyric</label>
+        <input
+          value={this.state.content}
+          onChange={event => this.setState({ content: event.target.value })}
+        />
+      </form>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation AddLyricToSong ($songId:ID, $content:String) {
+    addLyricToSong(songId:$songId, content:$content) {
+      id
+      title
+      lyrics {
+        id
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricCreate);
---
+++ client/components/LyricList.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
+class LyricList extends Component {
+  onLike(id) {
+    this.props.mutate({ variables: { id } });
+  }
+
+  renderLyrics() {
+    return this.props.lyrics.map(({ id, content, likes }) => {
+      return (
+        <li key={id} className="collection-item">
+          {content}
+          <i className="material-icons" onClick={() => this.onLike(id)}>
+            thumb_up
+          </i>
+          {likes}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return <ul className="collection">{this.renderLyrics()}</ul>;
+  }
+}
+
+const mutation = gql`
+  mutation LikeLyric($id:ID) {
+    likeLyric(id: $id) {
+      id
+      content
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
